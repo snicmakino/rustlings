e08f8f0053559da3a57803a512c3f588a8541766
snicmakino <snicmakino@gmail.com>
2021-01-04 07:54:35 +0900

    progress in 12/30 - 1/3
    - structとenum周りは何度か見直したほうが良さそう
    - stringとstrint sliceの違いはこの問題でかなり理解できた手応えがあるので、迷ったら見直す
    - その他は知っていることがメインだったのでそこまで気にしなくて良い

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,14 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn ..... {
+fn calculate_apple_price(bucks: i32) -> i32 {
+    if bucks < 40 {
+        return bucks * 2
+    }
+    bucks
+}
 
 // Don't modify this function!
 #[test]
